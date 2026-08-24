712ad5b8063e10612d7e902621dc217d1ec9e3c8
Dave Council <dcouncilcsu@gmail.com>
2026-08-24 11:52:54 -0600

    debugging of env var

--- server/api/FreshServiceAgentByEmail.ts
+++ server/api/FreshServiceAgentByEmail.ts
@@ -4,6 +4,17 @@ export default defineEventHandler(async (event) => {
         runtimeConfig = useRuntimeConfig(event),
         email = query.email;
 
+    function fastHash(str: String) {
+        let hash = 5381; // Starting prime number seed
+        
+        for (let i = 0; i < str.length; i++) {
+            // Equivalent to: hash * 33 + charCode
+            hash = (hash << 5) + hash + str.charCodeAt(i);
+        }
+        
+        return hash >>> 0; // Force convert to a 32-bit unsigned integer
+    }
+
     try {
         const response = await fetch(`https://coftc.freshservice.com/api/v2/agents?query=email:'${email}'`, {
             method: 'GET', // or 'POST', 'PUT', etc.
@@ -17,11 +28,12 @@ export default defineEventHandler(async (event) => {
         }
 
         let data = await response.json()
-        
+        // console.log('hash: ' + fastHash(btoa(runtimeConfig.fsApiKey)))
+
         return { success: true, data: data};
     } catch (err: any) {
-        console.log(err.message)
-        return { success: false, error: err.message }
+        // console.log(err.message)
+        return { success: false, error: 'key hash: ' +  fastHash(btoa(runtimeConfig.fsApiKey)) }
     } finally {
         console.log('done in the function');
     }

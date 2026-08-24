af5490cb717ac6808da2ef049721c65fddc33efe
Dave Council <dcouncilcsu@gmail.com>
2026-08-24 09:24:18 -0600

    test page with API call

--- app/app.vue
+++ app/app.vue
@@ -1,30 +1,3 @@
-<script setup lang="ts">
-
-const fetchManagerAgentFsData = async () => {
-  // let email = storeData.name;
-  let email: string = 'mnardi@fcgov.com'
-
-  try {
-    const managerAgentResponse = await useFetch('/api/FreshServiceAgentsByEmail', {
-      query: {
-        email: email
-      }
-    });
-
-    if (managerAgentResponse.status.value !== 'success') {
-      throw new Error(`HTTP Error! Response status: ${managerAgentResponse.status.value}`);
-    }
-
-    console.log(managerAgentResponse.data)
-
-  } catch (error: any) {
-    console.error('Fetch operation failed:', error.message);
-  }
-
-}
-fetchManagerAgentFsData();
-</script>
-
 <template>
   <div>
     <NuxtRouteAnnouncer />
---
+++ app/pages/test.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+
+const fetchManagerAgentFsData = async () => {
+  let email: string = 'mnardi@fcgov.com'
+
+  try {
+    const managerAgentResponse = await useFetch('/api/FreshServiceAgentByEmail', {
+      query: {
+        email: email
+      }
+    });
+
+    if (managerAgentResponse.status.value !== 'success') {
+      throw new Error(`HTTP Error! Response status: ${managerAgentResponse.status.value}`);
+    }
+
+    console.log(managerAgentResponse.data)
+
+  } catch (error: any) {
+    console.error('Fetch operation failed:', error.message);
+  }
+
+}
+fetchManagerAgentFsData();
+</script>
+
+<template>
+	<div>
+		<p>Test Page</p>
+	</div>
+</template>
